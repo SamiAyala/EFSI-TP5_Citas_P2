1ead3224d3027b212214c4131a9c492fa701ac55
SamiAyala <samirayala23@gmail.com>
2023-06-01 10:13:58 -0300

    borrados los console.log

--- src/componentes/Cita.js
+++ src/componentes/Cita.js
@@ -2,21 +2,20 @@ import './Cita.css';
 
 function Cita(props) {
     const eliminarCita = () => {
-        console.log(props.getCitas,props.id);
         let citas = props.getCitas();
         props.setCitas(
-                citas.filter(cita => cita.id === props.id)
+            citas.filter(cita => cita.id === props.id)
         )
     }
     return (
-        <div className = 'cita'>
-              <p>Mascota: <span>{props.mascota}</span></p>
-              <p>Dueño: <span>{props.dueño}</span></p>
-              <p>Fecha: <span>{props.fecha}</span></p>
-              <p>Hora: <span>{props.hora}</span></p>
-              <p>Sintomas: <span>{props.sintomas}</span></p>
-              <button className='button elimnar u-full-width' onClick={eliminarCita}>Eliminar ×</button>
-            </div>
+        <div className='cita'>
+            <p>Mascota: <span>{props.mascota}</span></p>
+            <p>Dueño: <span>{props.dueño}</span></p>
+            <p>Fecha: <span>{props.fecha}</span></p>
+            <p>Hora: <span>{props.hora}</span></p>
+            <p>Sintomas: <span>{props.sintomas}</span></p>
+            <button className='button elimnar u-full-width' onClick={eliminarCita}>Eliminar ×</button>
+        </div>
     )
 }
 
